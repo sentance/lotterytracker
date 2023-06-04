bf41cfa6f8b04d89568522f093666a8881b2435b
Oleh Bohdan <oleh.bohdan@mgm-tp.com>
2023-06-04 21:20:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCEgQa3iZKAHlQ2EdnrwuPp3RcQA_qBS8k",
+  authDomain: "lotterytracker.firebaseapp.com",
+  projectId: "lotterytracker",
+  storageBucket: "lotterytracker.appspot.com",
+  messagingSenderId: "307997671367",
+  appId: "1:307997671367:web:60ad890ea154e0fefca3e2",
+  measurementId: "G-5WLES1XS30",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
@@ -9,12 +31,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
